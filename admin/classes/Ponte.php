5fe5018478acc01f500f7ca847f0d21cb1f611a7
wedson santos <wedson_cross@hotmail.com>
2017-10-01 17:13:41 -0300

    correções da conexão

--- admin/classes/Ponte.php
+++ admin/classes/Ponte.php
@@ -1,9 +1,9 @@
 <?php
 class Ponte {
 
-    private static $Host = 'localhost';
+    private static $Host = '159.203.133.215';
     private static $User = 'root';
-    private static $Pass = '';
+    private static $Pass = '101514we';
     private static $Dbsa = 'boleto';
 
     private static $Connect = null;
